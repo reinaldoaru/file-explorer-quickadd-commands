d910003e2b35f0d10e35b50673cb5ddfda950e6b
Reinaldo Aru <reinaldo.aru@easydatgroup.com>
2024-05-20 18:58:28 -0400

    Principal feature added: Invoking Quick Add Choice from File Explorer, passing element path.

--- main.ts
+++ main.ts
@@ -1,81 +1,103 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile, TFolder } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface QuickAddCommandSettings {
+	id: string,
+	name: string,
+	display?: string
+}
+interface FolderSettings {
+	path: string,
+	commands: QuickAddCommandSettings[]
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+interface FileExplorerQuckAddCommansPluginSettings {
+	folders?: FolderSettings[];
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const DEFAULT_SETTINGS: FileExplorerQuckAddCommansPluginSettings = {
+	folders: [
+		{
+			path: '01-Projects',
+			commands: [
+				{
+					id: 'quickadd:choice:baf2d79c-ca7e-488f-852c-300935b32565',
+					name: 'New PARA',
+					display: 'Crear nuevo projecto...'
+				}
+			]
+		},
+		{
+			path: '02-Areas',
+			commands: [
+				{
+					id: 'quickadd:choice:baf2d79c-ca7e-488f-852c-300935b32565',
+					name: 'New PARA',
+					display: 'Crear nueva area...'
+				}
+			]
+		},
+		{
+			path: '03-Resources',
+			commands: [
+				{
+					id: 'quickadd:choice:baf2d79c-ca7e-488f-852c-300935b32565',
+					name: 'New PARA',
+					display: 'Crear nuevo recurso...'
+				}
+			]
+		}
+	]
+}
+
+export default class FileExplorerQuckAddCommansPlugin extends Plugin {
+	settings: FileExplorerQuckAddCommansPluginSettings;
+	protected quickAddAPI: any;
+
+	protected quickAdd() {
+		if (this.quickAddAPI == null) this.quickAddAPI = this.app.plugins.plugins["quickadd"].api;
+
+		return this.quickAddAPI;
+	}
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.registerEvent(this.app.workspace.on('file-menu', (menu, file, source) => {
+			if (this.settings.folders != null && this.settings.folders.length > 0) {
+				if (file instanceof TFolder) { 
+					this.settings.folders!.filter((folder: FolderSettings, i: number, array: FolderSettings[]) => {
+							let path = folder.path;
+							let starCount = 0;
+							let folderLevels;
+							let fileLevels;
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+							while (path.endsWith('/*')) {
+								starCount++;
+								path = path.slice(0, -2);
+							}
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+							folderLevels = path.split('/').length;
+							fileLevels = file.path.split('/').length - starCount
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+							return (file.path.startsWith(path) && fileLevels === folderLevels);
+							
+						}).forEach((folder: FolderSettings, i: number, array: FolderSettings[]) => {
+								folder.commands.forEach((command: QuickAddCommandSettings, i: number, array: QuickAddCommandSettings[]) => {
+										menu.addItem((item) => {
+											item
+												.setTitle(command.display != null ? command.display : command.name)
+												//.setIcon("folder-plus")
+												.onClick(async () => this.quickAdd().executeChoice(command.name, {path: file.path}));
+											});
+									})
+							})
+				} else if (file instanceof TFile) {
+
 				}
 			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		}));
 	}
 
 	onunload() {
@@ -83,7 +105,15 @@ export default class MyPlugin extends Plugin {
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		let settingsData = JSON.stringify(this.loadData());
+
+		if (settingsData == null || settingsData === '{}') {
+			settingsData = JSON.stringify(DEFAULT_SETTINGS);
+		}
+
+		this.settings = JSON.parse(settingsData);
+
+		console.log(JSON.stringify(this.settings));
 	}
 
 	async saveSettings() {
